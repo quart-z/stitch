dd2db6bd998d8da57b8ede55aceb9367a63f6221
quart-z <jtw2212@gmail.com>
2022-05-26 15:32:59 -0700

    Added gradients for background + keyframes

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
                   Github
                 </a>
               </li>
-              <img src="/sun.png" href="index.html" className=" sun_logo sun_logo_box hue-shift">
+              <img src="/sun.png" href="index.html" className=" sun_logo sun_logo_box">
               </img>
               
             </ul>
@@ -40,6 +40,14 @@ function App() {
       </header>
       <div className = "landing_page">
       {/* Content section , <Content /> */}
+
+        {/* Background gradients */}
+        <div className="gradient1">
+        </div>
+        <div className="gradient2">
+        </div>
+
+        {/* Title, description boxes and items */}
         <div className = "title_box">
           <h1 className= "title_item">
           Style your components with confidence
@@ -49,19 +57,19 @@ function App() {
           </h2>
         </div>
 
+        {/* Version Box, and items */}
         <div className = "version_content_box">
           <div className = "version_box">
-
             <img src="/logo512.png" href="index.html" className="App-logo version_item version_item_logo">
             </img>
             <p className = "version_item"> Stitches v10.0829 </p>
             <p className = "version_item"> I </p>
             <p className = "version_item"> See what's new -> </p>
-
           </div>
           <div className = "version_box_glow">
           </div>
         </div>
+
       </div>
     </body>
   );
